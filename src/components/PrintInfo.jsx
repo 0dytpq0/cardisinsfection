cb97dd38752c65eee68192af01152f0de5c25b43
0dytpq0 <rkfnahs12e@gmail.com>
2022-12-12 14:32:07 +0900

    대문자

---
+++ src/components/PrintInfo.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import "../App.css";
+import { Button, Row, Col, Divider } from "antd";
+import { useInfo } from "../store";
+import { ArrowRightOutlined } from "@ant-design/icons";
+
+export default function Printinfo({ printRef }) {
+  const { carinfo, areainfo, actorinfo, checkerinfo, waitingcar } = useInfo();
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth();
+  let date = today.getDate();
+
+  return (
+    <>
+      <div
+        className="printarea"
+        ref={printRef}
+        style={{
+          padding: "10px",
+          height: "80vh",
+        }}
+      >
+        <div className="printinfo">
+          <span>일련번호 - {carinfo?.PrintIndex} </span>
+          <h2 style={{ textAlign: "center" }}>소 독 필 증</h2>
+          <span>운전자 정보</span>
+          <Row className="innerinfo">
+            <Row>
+              <Col className="font">성명 : {carinfo?.Owner}</Col>
+            </Row>
+            <Row>
+              <Col className="font">차량 번호 : {carinfo?.Number}</Col>
+            </Row>
+            <Row>
+              <Col className="font">등록 번호 : {carinfo?.RegNumber}</Col>
+            </Row>
+            <Row>
+              <Col className="font">GPS 번호 : {carinfo?.GpsNumber}</Col>
+            </Row>
+
+            <Row>
+              <Col className="font">주소 : {carinfo?.Address}</Col>
+            </Row>
+            <Row>
+              <Col className="font">연락처 : {carinfo?.Phone}</Col>
+            </Row>
+            <Row>
+              <Col className="font">차량 목적 : {carinfo?.Purpose}</Col>
+            </Row>
+          </Row>
+          <Divider style={{ border: "1px solid rgba(0,0,0,0.8)" }} />
+          <span>소득내역</span>
+          <div className="innerinfo">
+            <Row>
+              <Col className="font">
+                소독일시 : {year}/{month}/{date}
+              </Col>
+            </Row>
+            <Row>
+              <Col className="font">
+                이동경로 :{" "}
+                {carinfo?.SPoint
+                  ? carinfo?.SPoint
+                  : "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0"}
+                <ArrowRightOutlined />
+                {carinfo?.EPoint}
+              </Col>
+            </Row>
+            <Row>
+              <Col className="font">소독내용 : {areainfo.DContent}</Col>
+            </Row>
+          </div>
+          <Divider style={{ border: "1px solid rgba(0,0,0,0.8)" }} />
+          <span>소득 실시자</span>
+          <div className="innerinfo">
+            <Row>
+              <Col className="font">소독 지역 : {areainfo.Area}</Col>
+            </Row>
+            <Row>
+              <Col className="font">소독 장소명 : {areainfo.PointName}</Col>
+            </Row>
+            <Row>
+              <Col className="font">소속 : {actorinfo.Attached}</Col>
+            </Row>
+            <Row>
+              <Col className="font">직급 : {actorinfo.Position}</Col>
+            </Row>
+            <Row>
+              <Col className="font">성명 : {actorinfo.Name}</Col>
+            </Row>
+          </div>
+          <Divider style={{ border: "1px solid rgba(0,0,0,0.8)" }} />
+          <h3>
+            가축전염병예방법 제 17조 제 3항에 따라 위와 같이 소독을 실시하였음을
+            증명합니다
+          </h3>
+          <div className="checkman">
+            소독 실시 확인자
+            <Row className="checkman__font">
+              <Col className="font">소속 - {checkerinfo.Attached}</Col>
+              <Col className="font">직급 - {checkerinfo.Position}</Col>
+              <Col className="font auth_name">성명 - {checkerinfo.Name}</Col>
+              <img
+                className="auth_img"
+                src="http://127.0.0.1:4000/images/auth.png"
+              />
+            </Row>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
